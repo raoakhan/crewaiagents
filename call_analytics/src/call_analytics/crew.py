0a71c91d3597b9b49b54e376a66e51ec70d9deda
Akmal Khan <akmal_shahbaz@yahoo.com>
2025-07-17 00:54:16 +0500

    chore(call_analytics): remove listener dependency not available

--- call_analytics/src/call_analytics/crew.py
+++ call_analytics/src/call_analytics/crew.py
@@ -99,16 +99,13 @@ class CallAnalytics():
         # To learn how to add knowledge sources to your crew, check out the documentation:
         # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
-        from call_analytics.listeners import TraceListener  # noqa: WPS433
-
         from call_analytics.tools import TranscribeTool, SentimentTool, InsightTool  # noqa: WPS433
 
         return Crew(
             agents=self.agents,  # Automatically created by the @agent decorator
             tasks=self.tasks,  # Automatically created by the @task decorator
             tools=[TranscribeTool(), SentimentTool(), InsightTool()],  # instances, names already set to snake_case
-            listeners=[TraceListener()],
             process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            verbose=True
+            # process=Process.hierarchical  # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
